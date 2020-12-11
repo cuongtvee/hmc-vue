25cd94ff2d41ed87e61443d243cf6b3239436d7e
MrTimscampi <julien.machiels@protonmail.com>
2020-12-11 15:03:50 +0100

    Revert "build(nuxt.config.ts): add options to improve build perfs"
    This reverts commit cc22d250022d0cd425ca0226581c98527d295271.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -205,8 +205,6 @@ const config: NuxtConfig = {
   /*
    ** Build configuration
    ** See https://nuxtjs.org/api/configuration-build/
-
-   ** Build perfs options come from this https://github.com/nuxt/nuxt.js/issues/5131#issuecomment-468231314
    */
   build: {
     // @ts-ignore -- Undocumented options
@@ -230,10 +228,7 @@ const config: NuxtConfig = {
         ];
       }
     },
-    transpile: ['@nuxtjs/auth', 'vee-validate/dist/rules'],
-    cache: true,
-    hardSource: true,
-    parallel: true
+    transpile: ['@nuxtjs/auth', 'vee-validate/dist/rules']
   },
 
   /**
